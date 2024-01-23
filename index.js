4a67b1634db07aa46bb838f6bba9ce2eeda1acba
Anestasia Hoffmann <anestasiahoffmann@gmail.com>
2024-01-23 11:50:08 +0200

    firebase removed

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const port = 4000;
 const express = require("express");
-const app = express();
+const app = express()
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+
 
 
 
